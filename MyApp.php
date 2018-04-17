9487b0f0e90dc7fd37381a7486e9611bc539f6e4
atanasovanadya <nadi_29@abv.bg>
2018-04-17 13:18:59 +0300

    initial commit

---
+++ MyApp.php
@@ -0,0 +1,48 @@
+<?php // define nested array 
+$students = array(
+	array(
+		'id' => '1',
+		'name' => 'Raymond Rabbit',
+		'tel' => '1234567',
+		'email' => 'ray@bunnyplanet.in',
+		'points' => '120',
+	),     
+	array(
+		'id' => '2',
+		'name' => 'David Duck',
+		'tel' => '8562904',
+		'email' => 'dduck@duckpond.corp',
+		'points' => '98',
+	),
+	array(
+		'id' => '3',
+		'name' => 'Harold Horse',
+		'tel' => '5942033',
+		'email' => 'kingharold@farmersmarket.horsestuff.com',
+		'points' => '140',
+	),
+	array(
+		'id' => '4',
+		'name' => 'Jim Pembroke',
+		'tel' => '4533322',
+		'email' => 'JimP@some.com',
+		'points' => '111',
+	),
+	array(
+		'id' => '5',
+		'name' => 'Ben Duke',
+		'tel' => '3344556',
+		'email' => 'BenDuke@smarket.com',
+		'points' => '145',
+	) );
+//Grades 
+$grades = array(
+	array(
+		'student_id' => '1',
+		'course_id' => 'c1',
+		'grade' => '2'
+	),
+	array(
+		'student_id' => '2',
+		'course_id' => 'c2',
+		'grade' => '4' 
