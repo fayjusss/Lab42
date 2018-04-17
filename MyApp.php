ff801d4db6b74f249ac2a8674124486d499e3c89
Fayjus Salehin <fsalehin@live.com>
2018-04-17 13:14:19 +0300

    Initial Commit

---
+++ MyApp.php
@@ -0,0 +1,58 @@
+<?php
+// define nested array
+$students = array(
+  array(
+  'id' => '1',
+  'name' => 'Raymond Rabbit', 'tel' => '1234567',
+  'email' => 'ray@bunnyplanet.in', 'points' => '120',
+  ), array(
+  'id' => '2',
+  'name' => 'David Duck',
+  'tel' => '8562904',
+  'email' => 'dduck@duckpond.corp', 'points' => '98',
+  ), array(
+  'id' => '3',
+  'name' => 'Harold Horse',
+  'tel' => '5942033',
+  'email' => 'kingharold@farmersmarket.horsestuff.com', 'points' => '140',
+  ), array(
+  'id' => '4',
+  'name' => 'Jim Pembroke', 'tel' => '4533322',
+  'email' => 'JimP@some.com', 'points' => '111',
+  ), array(
+  'id' => '5',
+  'name' => 'Ben Duke',
+  'tel' => '3344556',
+  'email' => 'BenDuke@smarket.com', 'points' => '145',
+  )
+);
+//Grades
+$grades = array(
+  array(
+  'student_id' => '1', 'course_id' => 'c1', 'grade' => '2'
+  ), array(
+  'student_id' => '2', 'course_id' => 'c2', 'grade' => '4'
+  ), array(
+  'student_id' => '1', 'course_id' => 'c3', 'grade' => '3'
+  ), array(
+  'student_id' => '5', 'course_id' => 'c4', 'grade' => '2'
+  ), array(
+  'student_id' => '4', 'course_id' => 'c3', 'grade' => '4'
+  ),array(
+  'student_id' => '3', 'course_id' => 'c3', 'grade' => '3'
+  ), array(
+  'student_id' => '2', 'course_id' => 'c5', 'grade' => '4'
+  ), array(
+  'student_id' => '1', 'course_id' => 'c6', 'grade' => '5'
+  ), array(
+  'student_id' => '3', 'course_id' => 'c4', 'grade' => '3'
+  ), array(
+  'student_id' => '4', 'course_id' => 'c12', 'grade' => '5'
+  ), array(
+  'student_id' => '1', 'course_id' => 'c13', 'grade' => '4'
+  ),
+  array(
+  'student_id' => '5', 'course_id' => 'c14', 'grade' => '3'
+  ),
+);
+?>
